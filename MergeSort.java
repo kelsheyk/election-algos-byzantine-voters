314696327556b15082f0605aa8df7b3f4cd5e7ef
howinator <howiebenefiel@gmail.com>
2018-11-26 21:51:58 -0600

    Start on mergesort

---
+++ MergeSort.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class MergeSort {
+    public MergeSort(ArrayList<ArrayList<String>> voterData) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
